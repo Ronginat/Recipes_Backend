54f09b452ec3fc1eac6a01075ccce9ba91b61212
ronginat <ronginat51@gmail.com>
2018-09-17 23:54:45 +0300

    organize in folders

---
+++ s3_events/Lambda_ContentUploadedToS3.js
@@ -0,0 +1,162 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: process.env['REGION']});
+const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+const docClient = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
+
+function dateToString() {
+    const date = new Date();
+    var day = date.getUTCDate();
+    var month = date.getUTCMonth() + 1;
+    var year = date.getUTCFullYear();
+
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+
+    return '' + year + '-' + (month <= 9 ? '0' + month : month) + '-' + (day <= 9 ? '0' + day : day)
+            + ' ' + (hours <= 9 ? '0' + hours : hours) + ':' + (minutes <= 9 ? '0' + minutes : minutes)
+            + ':' + (seconds <= 9 ? '0' + seconds : seconds);
+}
+
+function getIdFromFileName(name) {
+    const dot = name.lastIndexOf(".");
+    let temp = name.substring(0, dot);
+    return temp.split("---")[1];
+}
+
+function putItemInRecipes(pendItem) {
+    const date = dateToString();
+
+    const params = {
+        TableName: process.env['RECIPE_TABLE'],
+        Item: {
+            'id' : pendItem.id,
+            'sharedKey': process.env['SHARED_KEY'],
+            'name' : pendItem.name,
+            'description': pendItem.description,
+            'uploader': pendItem.username,
+            'categories': pendItem.categories,
+            'fileName': pendItem.fileName,
+            'createdAt': date,
+            'lastModifiedAt': date,
+            'likes': 0,
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        // Call DynamoDB to add the item to the table
+        docClient.put(params, function(err, data) {
+            if (err) {
+                console.log("Error recipe PUT", err);
+                return reject(err);
+            } 
+            else {
+                console.log("Success recipe PUT", data);
+                return resolve(data.Item);
+            }
+        });
+    });
+}
+
+function removeFromPending(key) {
+    let params = {
+        "TableName": process.env['RECIPE_PEND_TABLE'],
+        "Key": {
+            "id": key
+        },
+        "ReturnValues": "ALL_OLD"
+    };
+
+    return new Promise((resolve, reject) => {
+        // Call DynamoDB to add the item to the table
+        docClient.delete(params, function(err, data) {
+            if (err) {
+                console.log("Error pend DELETE", err);
+                return reject(err);
+            } 
+            else {
+                if(data['Attributes'] == null) {
+                    reject("uploaded file not found in pending table");
+                }
+                else {
+                    console.log("Success pend DELETE", data);
+                    return resolve(data['Attributes']);
+                }
+            }
+        });
+    });
+}
+
+function deleteFromS3(bucket, key) {
+    let params = {
+        Bucket: bucket, 
+        Key: key
+    };
+
+    return new Promise((resolve, reject) => {
+        // Call DynamoDB to add the item to the table
+        s3.deleteObject(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+                reject(err);
+            }
+            else {
+                console.log(data); // successful response
+                resolve(data);
+            }
+        });
+    });
+}
+
+function deletePendingImagesFromS3(images) {
+    let array = [];
+    let params = {
+        Bucket: process.env['BUCKET'],
+        Delete: {
+            Quiet: false,
+        },
+    };
+    images.forEach(function(element, index, arr) {
+        array.push({
+            Key: element
+        });
+    });
+    params.Delete['Objects'] = array;
+
+    return new Promise((resolve, reject) => {
+        // Call DynamoDB to add the item to the table
+        s3.deleteObjects(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+                reject(err);
+            }
+            else {
+                console.log(data); // successful response
+                resolve(data);
+            }
+        });
+    });
+}
+
+exports.handler = async function(event, context) {
+    let record = event['Records'][0]['s3'];
+    let uploadedName = record['object']['key'];
+    let bucket = record['bucket']['name'];
+
+    const id = getIdFromFileName(uploadedName);
+    try {
+        let removedPend = await removeFromPending(id);
+        let updatedRecipeItem = await putItemInRecipes(removedPend);
+        let removedImages = await deletePendingImagesFromS3(removedPend['pendImages']);
+        
+        console.log("recipe upload successfully.\n" + JSON.stringify(updatedRecipeItem));
+
+    } catch(err) {
+        console.log("error when uploading recipe.\n" + err);
+        console.log("deleting file from bucket...");
+        await deleteFromS3(bucket, uploadedName);
+    }
+};
---
+++ s3_events/Lambda_FoodUploadedToS3.js
@@ -0,0 +1,151 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: process.env['REGION']});
+const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+const docClient = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+// const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+
+
+// function getUsername(token){
+//     let params = {
+//         AccessToken: token
+//       };
+//     return new Promise((resolve, reject) => {
+//         cognitoidentityserviceprovider.getUser(params, function(err, data) {
+//             if (err) {
+//                 console.log(err); // an error occurred
+//                 return reject(err);
+//             }
+//             else {
+//                 console.log(data); // successful response
+//                 return resolve(data.Username);
+//             }    
+//         });
+//     });
+// }
+
+function dateToString() {
+    const date = new Date();
+    var day = date.getUTCDate();
+    var month = date.getUTCMonth() + 1;
+    var year = date.getUTCFullYear();
+
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+
+    return '' + year + '-' + (month <= 9 ? '0' + month : month) + '-' + (day <= 9 ? '0' + day : day)
+            + ' ' + (hours <= 9 ? '0' + hours : hours) + ':' + (minutes <= 9 ? '0' + minutes : minutes)
+            + ':' + (seconds <= 9 ? '0' + seconds : seconds);
+}
+
+function updateItemInRecipes(pendItem) {
+    const date = dateToString();
+
+    let params = {
+        TableName: process.env['RECIPE_TABLE'],
+        Key: {
+            "id" : {"S": pendItem.id},
+            "sharedKey": {"S": process.env['SHARED_KEY']},
+        },
+        UpdateExpression : "SET #attrList = list_append(#attrList, :listValue), #attrDate = :dateValue",
+        ExpressionAttributeNames : {
+            "#attrList" : "recipeFiles",
+            "#attrDate": "lastModifiedAt"
+        },
+        ExpressionAttributeValues : {
+            ":listValue": {"L": [ { "S": pendItem.fileName }]},
+            ":dateValue": {"S": date}
+        },
+        ReturnValues: "UPDATED_NEW "
+    };
+
+    return new Promise((resolve, reject) => {
+        // Call DynamoDB to add the item to the table
+        ddb.updateItem(params, function(err, data) {
+            if (err) {
+                console.log("Error recipe UPDATE", err);
+                return reject(err);
+            } 
+            else {
+                if(data['Attributes'] == null) {
+                    reject("error linking the file with recipe in db");
+                }
+                else {
+                    console.log("Success recipe UPDATE", data);
+                    return resolve(data['Attributes']);
+                }
+            }
+        });
+    });
+}
+
+function removeFromPending(key) {
+    let params = {
+        "TableName": process.env['PEND_IMG_TABLE'],
+        "Key": {
+            "fileName": key
+        },
+        "ReturnValues": "ALL_OLD"
+    };
+
+    return new Promise((resolve, reject) => {
+        // Call DynamoDB to add the item to the table
+        docClient.delete(params, function(err, data) {
+            if (err) {
+                console.log("Error pend DELETE", err);
+                return reject(err);
+            } 
+            else {
+                if(data['Attributes'] == null) {
+                    reject("uploaded file not found in pending table");
+                }
+                else {
+                    console.log("Success pend DELETE", data);
+                    return resolve(data['Attributes']);
+                }
+            }
+        });
+    });
+}
+
+function deleteFromS3(bucket, key) {
+    let params = {
+        Bucket: bucket, 
+        Key: key
+    };
+
+    return new Promise((resolve, reject) => {
+        // Call DynamoDB to add the item to the table
+        s3.deleteObject(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+                reject(err);
+            }
+            else {
+                console.log(data); // successful response
+                resolve(data);
+            }
+        });
+    });
+    
+}
+
+exports.handler = async function(event, context) {
+    let record = event['Records'][0]['s3'];
+    let uploadedName = record['object']['key'];
+    let bucket = record['bucket']['name'];
+
+    try {
+        let removedPend = await removeFromPending(uploadedName);
+        let updatedRecipeItem = await updateItemInRecipes(removedPend);
+        
+        console.log("recipe upload successfully.\n" + JSON.stringify(updatedRecipeItem));
+
+    } catch(err) {
+        console.log("error when uploading recipe.\n" + err);
+        console.log("deleting file from bucket...");
+        await deleteFromS3(bucket, uploadedName);
+    }
+};
